f99edc91f204eda66816ead6bd0bf30f8641c154
scheric <38077357+scheric@users.noreply.github.com>
2019-12-25 13:22:57 +0100

    Add meaningful icons

--- ems_esp/constants.py
+++ ems_esp/constants.py
@@ -4,121 +4,121 @@ CONSTANTS = {
     "return temp sensor": {
         "value": "retTemp",
         "name": "Return Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:thermometer-chevron-up",
         "unit": "°C",
     },
     "flow temp sensor": {
         "value": "curFlowTemp",
         "name": "Current Flow Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:thermometer-chevron-down",
         "unit": "°C",
     },
     "warm water flow sensor": {
         "value": "wWCurTmp",
         "name": "Current Warm Water Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:thermometer-chevron-down",
         "unit": "°C",
     },
     "warm water sensor": {
         "value": "boilTemp",
         "name": "Current Boiler Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:water-boiler",
         "unit": "°C",
     },
     "thermostat sensor": {
         "value": "thermostat_currtemp",
         "name": "Current Room Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:home-thermometer",
         "unit": "°C",
     },
     "thermostat setting": {
         "value": "thermostat_seltemp",
         "name": "Current Set Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:thermostat",
         "unit": "°C",
     },
     "warm water setting": {
         "value": "wWSelTemp",
         "name": "Current Warm Water Selected Temperature",
-        "icon": "mdi:flash",
+        "icon": "mdi:thermometer-high",
         "unit": "°C",
     },
     "burner power setting": {
         "value": "selBurnPow",
         "name": "Burner Max Power",
-        "icon": "mdi:flash",
+        "icon": "mdi:fire",
         "unit": "%",
     },
     "burner power sensor": {
         "value": "curBurnPow",
         "name": "Burner current power",
-        "icon": "mdi:flash",
+        "icon": "mdi:fire",
         "unit": "%",
     },
     "pump modulation sensor": {
         "value": "pumpMod",
         "name": "Pump Modulation",
-        "icon": "mdi:flash",
+        "icon": "mdi:rotate-left",
         "unit": "%",
     },
     "system pressure sensor": {
         "value": "sysPress",
         "name": "System Pressure",
-        "icon": "mdi:flash",
+        "icon": "mdi:water-percent",
         "unit": "bar",
     },
     "flow rate sensor": {
         "value": "wWCurFlow",
         "name": "Tapwater Flow Rate",
-        "icon": "mdi:flash",
+        "icon": "mdi:water-pump",
         "unit": "l/min",
     },
     "tap water mode": {
         "value": "tapwaterActive",
         "name": "Tap Water",
-        "icon": "mdi:flash",
+        "icon": "mdi:water-pump",
         "unit": None,
     },
     "warm water active mode": {
         "value": "wWActivated",
         "name": "Warm Water activated",
-        "icon": "mdi:flash",
+        "icon": "mdi:water-outline",
         "unit": None,
     },
     "3-way valve mode": {
         "value": "wWHeat",
         "name": "3-way valve",
-        "icon": "mdi:flash",
+        "icon": "mdi:valve",
         "unit": None,
     },
     "active heating mode": {
         "value": "heatingActive",
         "name": "Heating",
-        "icon": "mdi:flash",
+        "icon": "mdi:radiator",
         "unit": None,
     },
     "gas valve mode": {
         "value": "burnGas",
         "name": "gas valve",
-        "icon": "mdi:flash",
+        "icon": "mdi:gas-cylinder",
         "unit": None,
     },
     "boiler pump mode": {
         "value": "heatPmp",
         "name": "boiler pump",
-        "icon": "mdi:flash",
+        "icon": "mdi:rotate-left",
         "unit": None,
     },
     "boiler circulation pump mode": {
         "value": "wWCirc",
         "name": "boiler circulation pump",
-        "icon": "mdi:flash",
+        "icon": "mdi:rotate-left",
         "unit": None,
     },
     "fantilator mode": {
         "value": "fanWork",
         "name": "fantilator",
-        "icon": "mdi:flash",
+        "icon": "mdi:fan",
         "unit": None,
     },
     "ignition mode": {
@@ -130,15 +130,7 @@ CONSTANTS = {
     "boiler service code": {
         "value": "ServiceCode",
         "name": "Service Code",
-        "icon": "mdi:flash",
+        "icon": "mdi:face-agent",
         "unit": None,
     },
 }
-
-'''
-  - platform: mqtt
-    state_topic: 'home/ems-esp/boiler_data'
-    name: 'Return temperature'
-    unit_of_measurement: '°C'
-    value_template: '{{ value_json.retTemp }}'
-'''
