50d12dc7fefa1bad82e53c29485596af74f5a831
scheric <38077357+scheric@users.noreply.github.com>
2020-03-07 21:11:32 +0100

    cleanup

--- ems_esp/sensor.py
+++ ems_esp/sensor.py
@@ -13,34 +13,17 @@ from homeassistant.const import (
 
 import json
 
-DOMAIN = "ems_esp"
+DOMAIN = "xxxx"
 
-from .constants import CONSTANTS_SENSORS_BOILER, CONSTANTS_BOILER_POWER, CONSTANTS_SENSORS_SENSORS, CONSTANTS_SENSORS_THERMOSTAT
+from .constants import [CONSTANTS IMPORT]
 
 CONF_BASE = "base_topic"
-CONF_THERMOSTAT = "thermostat"
-CONF_BOILER = "boiler"
-CONF_SENSORS = "sensor"
-CONF_SHOWER_DATA = "shower"
-CONF_SOLAR_DATA = "solar"
-CONF_HEATPUMP_DATA = "heatpump"
-CONF_HEARTBEAT = "heartbeat"
-CONF_MIXING_DATA = "mixer"
 
-CONF_POWER = "boiler_power"
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default="EMS_ESP"): cv.string,
-    vol.Optional(CONF_BASE, default="home"): cv.string,
     vol.Optional(CONF_THERMOSTAT, default=False): cv.boolean,
-    vol.Optional(CONF_BOILER, default=True): cv.boolean,
     vol.Optional(CONF_SENSORS, default=0): cv.positive_int,
-    vol.Optional(CONF_SHOWER_DATA, default=False): cv.boolean,
-    vol.Optional(CONF_SOLAR_DATA, default=False): cv.boolean,
-    vol.Optional(CONF_HEATPUMP_DATA, default=False): cv.boolean,
-    vol.Optional(CONF_HEARTBEAT, default=False): cv.boolean,
-    vol.Optional(CONF_MIXING_DATA, default=False): cv.boolean,
-    
     vol.Optional(CONF_POWER, default=0): vol.Coerce(float),
 })
 
@@ -48,7 +31,7 @@ _LOGGER = logging.getLogger(__name__)
         #_LOGGER.error(f"sensor: {sensor}")
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
-    """Set up ems_esp sensors."""
+    """Set up XXX sensors."""
     
     sensors = []
      
@@ -57,65 +40,18 @@ async def async_setup_platform(hass, config, async_add_entities, discovery_info=
         for sensor in CONSTANTS_SENSORS_THERMOSTAT:
             sensors.append(EMS_ESPSensor(config, CONSTANTS_SENSORS_THERMOSTAT[sensor].get("name"), CONSTANTS_SENSORS_THERMOSTAT[sensor].get("unit") , CONSTANTS_SENSORS_THERMOSTAT[sensor].get("icon"), CONSTANTS_SENSORS_THERMOSTAT[sensor].get("value"), CONSTANTS_SENSORS_THERMOSTAT[sensor].get("topic")))
 
-    #add boiler sensors DONE
-    if config[CONF_BOILER]:
-        for sensor in CONSTANTS_SENSORS_BOILER:
-            sensors.append(EMS_ESPSensor(config, CONSTANTS_SENSORS_BOILER[sensor].get("name"), CONSTANTS_SENSORS_BOILER[sensor].get("unit") , CONSTANTS_SENSORS_BOILER[sensor].get("icon"), CONSTANTS_SENSORS_BOILER[sensor].get("value"), CONSTANTS_SENSORS_BOILER[sensor].get("topic")))
-
-    # add sensor sensors DONE
-    if config[CONF_SENSORS]:
-        for ammount in range(1, (config[CONF_SENSORS]+1)):
-            for sensor in CONSTANTS_SENSORS_SENSORS:
-                sensors.append(EMS_ESPSensor(config, CONSTANTS_SENSORS_SENSORS[sensor].get("name") + " " + str(ammount), CONSTANTS_SENSORS_SENSORS[sensor].get("unit") , CONSTANTS_SENSORS_SENSORS[sensor].get("icon"), CONSTANTS_SENSORS_SENSORS[sensor].get("value") + str(ammount), CONSTANTS_SENSORS_SENSORS[sensor].get("topic")))
-
-    #add shower sensors
-    if config[CONF_SHOWER_DATA]:
-        for sensor in CONSTANTS_BOILER_POWER:
-            pass
-            #sensors.append(EMS_ESPSensor(config, CONSTANTS_BOILER_POWER[sensor].get("name"), CONSTANTS_BOILER_POWER[sensor].get("unit") , CONSTANTS_BOILER_POWER[sensor].get("icon"), CONSTANTS_BOILER_POWER[sensor].get("value"), CONSTANTS_BOILER_POWER[sensor].get("topic")))
-
-    #add solar sensors
-    if config[CONF_SOLAR_DATA]:
-        for sensor in CONSTANTS_BOILER_POWER:
-            pass
-            #sensors.append(EMS_ESPSensor(config, CONSTANTS_BOILER_POWER[sensor].get("name"), CONSTANTS_BOILER_POWER[sensor].get("unit") , CONSTANTS_BOILER_POWER[sensor].get("icon"), CONSTANTS_BOILER_POWER[sensor].get("value"), CONSTANTS_BOILER_POWER[sensor].get("topic")))
-
-    #add heatpump sensors
-    if config[CONF_HEATPUMP_DATA]:
-        for sensor in CONSTANTS_BOILER_POWER:
-            pass
-            #sensors.append(EMS_ESPSensor(config, CONSTANTS_BOILER_POWER[sensor].get("name"), CONSTANTS_BOILER_POWER[sensor].get("unit") , CONSTANTS_BOILER_POWER[sensor].get("icon"), CONSTANTS_BOILER_POWER[sensor].get("value"), CONSTANTS_BOILER_POWER[sensor].get("topic")))
-
-    #add heartbeat sensors
-    if config[CONF_HEARTBEAT]:
-        for sensor in CONSTANTS_BOILER_POWER:
-            pass
-            #sensors.append(EMS_ESPSensor(config, CONSTANTS_BOILER_POWER[sensor].get("name"), CONSTANTS_BOILER_POWER[sensor].get("unit") , CONSTANTS_BOILER_POWER[sensor].get("icon"), CONSTANTS_BOILER_POWER[sensor].get("value"), CONSTANTS_BOILER_POWER[sensor].get("topic")))
-
-    #add mixer sensors
-    if config[CONF_MIXING_DATA]:
-        for sensor in CONSTANTS_BOILER_POWER:
-            pass
-            #sensors.append(EMS_ESPSensor(config, CONSTANTS_BOILER_POWER[sensor].get("name"), CONSTANTS_BOILER_POWER[sensor].get("unit") , CONSTANTS_BOILER_POWER[sensor].get("icon"), CONSTANTS_BOILER_POWER[sensor].get("value"), CONSTANTS_BOILER_POWER[sensor].get("topic")))
-
-    #add boiler power sensor DONE
-    if config[CONF_POWER]:
-        for sensor in CONSTANTS_BOILER_POWER:
-            sensors.append(EMS_ESPSensor(config, CONSTANTS_BOILER_POWER[sensor].get("name"), CONSTANTS_BOILER_POWER[sensor].get("unit") , CONSTANTS_BOILER_POWER[sensor].get("icon"), CONSTANTS_BOILER_POWER[sensor].get("value"), CONSTANTS_BOILER_POWER[sensor].get("topic")))
-
     
     async_add_entities(sensors)
 
 
 class EMS_ESPSensor(Entity):
-    """Representation of a ems-esp sensor that is updated via MQTT."""
+    """Representation of a XXX sensor that is updated via MQTT."""
 
     def __init__(self, config, Name, Unit, Icon, Value, Topic):
         """Initialize the sensor."""
 
         self._base  = config[CONF_BASE]
         self._Name = config[CONF_NAME] + " "
-        self._boiler_power = config[CONF_POWER]
         
         #self._topic = self._base + "/ems-esp/boiler_data"
         
@@ -151,6 +87,7 @@ class EMS_ESPSensor(Entity):
         
         self._out = self._in
         
+        #calculation example
         if self._name == "current burner power":
             try:
                 self._out = round(((self._boiler_power/100) * self._in),1)
